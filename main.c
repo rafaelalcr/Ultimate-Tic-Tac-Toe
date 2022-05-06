e559e977b4f3b857fadf056104d79855a2c18712
Rafaela <rafaela99fs@hotmail.com>
2022-05-06 17:28:26 +0100

    game with a computer working

--- main.c
+++ main.c
@@ -5,3 +5,9 @@ int main() {
 
     return 0;
 }
+
+// TODO
+//  -> antes de cada jogada, perguntar ao utilizador se quer ver as jogadas anteriores
+//  -> guardar o jogo num ficheiro binário quando há interrupção
+//  -> guardar o jogo num ficheiro de texto quando o jogo acabar
+//  -> a sucessão de jogadas realizadas devem ser mantidas numa lista ligada simples
